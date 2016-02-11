b46ecde5270e45ff80a693db05ecee81831ffdb0
karolzdebel <kzdebel@mail.uoguelph.ca>
2016-02-10 20:52:09 -0500

    Updating progress

--- A1/program.c
+++ A1/program.c
@@ -1,56 +1,115 @@
 
+#ifndef __SHELL_H
+#define __SHELL_H
 #include "shell.h"
-#define DEBUG
+#endif
+
+static void freeVars(int outinType,char **cmd,char ***args,
+	char **backcmd,char **pipecmd,char ***pipeargs
+	,char **redirName){
+
+	if (outinType == __PIPE_OUTPUT){
+		free(*pipecmd);
+		freeArgs(pipeargs);
+	}
+	else if (outinType == __REDIR_FFILE
+		|| outinType == __REDIR_TFILE){
+		free(*redirName);
+	}
+	if (isBackCmd(*cmd)){
+		free(*backcmd);
+	}
+	free(*cmd);
+	freeArgs(args);
+}
 
 int main(int argv,char **argc){
-	static char **args = NULL;
-	char input[MAX_INPUT];
-	char *command = NULL;
-	pid_t childpid;
-	int status = 0;
+	char input[MAX_INPUT],*redirName = NULL;
+	char *cmd=NULL,*backcmd=NULL,*pipecmd=NULL;
+	char **args=NULL,**pipeargs=NULL,**dirs;
+	int status = 0,i=0,outinType;
+	Path *path=NULL;
 
-	/*Execute command in child process*/
+	childpids = NULL;
+	pidCount = 0;
+
+	initializePath(&path);
+	/*Get all directories inside path*/
+	dirs = getDirs();
+	/*Store all directories inside path linked list*/
+	while(dirs[i]){
+		addDir(path,dirs[i]);
+		free(dirs[i]);
+		i++;
+	}
+	free(dirs);
+
+	/*Initialize childpid to a non zero value*/
 
 	while (1){
 		printf("> ");
-		fgets(input, MAX_INPUT, stdin);
-
+		while(!fgets(input,MAX_INPUT,stdin)){;}
 		/*Empty input*/
 		if (isEmpty(input) == 0){
 			continue;
 		}
-
-		/*Free any memory from the previous input*/
-		if (command != NULL && args != NULL){
-			free(command);
-			freeArgs(&args);
-		}
 		
 		/*Get command and arguments*/
-		command = getCommand(input);
+		cmd = getCmd(input);
 		args = getArgs(input);
-		if (strcmp(command,"exit") == 0){
+		outinType = getOutInType(input);
+
+		/*Remove & from cmd*/
+		if (isBackCmd(cmd)){
+			backcmd = getBackCmd(cmd);
+		}
+		/*Get the name of which file redirection
+		  is occuring to/from*/
+		if (outinType == __REDIR_TFILE
+				|| outinType == __REDIR_FFILE){
+				redirName = getRedirName(input,outinType);
+		}
+		/*Get the second commands name
+		and arguments*/
+		else if(outinType == __PIPE_OUTPUT){
+			pipecmd = getPipeCmd(input);
+			pipeargs = getPipeArgs(input,pipecmd);
+		}
+
+		/*Exit*/
+		if (strcmp(cmd,"exit") == 0){
 			printf("Logout\n\n[Process Completed]\n");
-			free(command);
-			freeArgs(&args);
+
+			/*Free all memory*/
+			freeVars(outinType,&cmd,&args,&backcmd
+				,&pipecmd,&pipeargs,&redirName);
+			freePath(&path);
 			exit(1);
 		}
+		/*Run in background asynchronously*/
+		else if (isBackCmd(cmd)){
+			execBackCmd(backcmd,args,outinType,redirName);
+		}
+		/*Run two commands by piping first output
+		 to the second commands input*/
+		else if (outinType == __PIPE_OUTPUT){
+			execPipeCmd(cmd,args,pipecmd,pipeargs);
+		}
+		/*Run the command synchronously*/
 		else{
-			childpid = fork();
-			waitpid(childpid,&status,0);
-			/*Let the child process execute the command*/
-			if (childpid == 0){
-				execvp(command,args);
-
-				/*Free memory if error returned*/
-				free(command);
-				freeArgs(&args);
-				exit(errno);
-			}
+			/*Execute command*/			
+			execCmd(cmd,args,outinType,redirName
+				,&status);
 			if (WEXITSTATUS(status)){
-				printf("Abnormal return code: %d\n",WEXITSTATUS(status));
+				printf("Abnormal command, return code: %d\n",WEXITSTATUS(status));
 			}
 		}
+
+		/*Free all memory*/
+		freeVars(outinType,&cmd,&args,&backcmd
+			,&pipecmd,&pipeargs,&redirName);
 	}
+	freePath(&path);
+
 	return 0;
 }
